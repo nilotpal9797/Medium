470b5cb13cccc54df31cb4fc25001bd3a9f3424c
Nilotpal Mrinal <nilotpalmrinal9797@gmail.com>
2019-09-11 12:25:34 +0530

    Update and rename Sort.cpp to SortArra.cpp

--- Sort.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
- vector<int> v;
- sort(v.begin(),v.end());
- return 0;
-}
---
+++ SortArra.cpp
@@ -0,0 +1,5 @@
+// If we have the input as an array  arr
+sort(arr,arr+n);
+
+//If we have a vector  v then something like this
+sort(v.begin(),v.end());
